c238dc06e3a46996b030e4340acac37e2085f9cd
RodrigoSHL <rodrigo.catalan@tcit.cl>
2024-06-21 12:46:07 -0400

    CHORE: changes un api path. add public path for test

--- WebAppNetFramework/Controllers/ProductsController.cs
+++ WebAppNetFramework/Controllers/ProductsController.cs
@@ -9,7 +9,7 @@ namespace WebAppMvc.Controllers
 {
     public class ProductsController : Controller
     {
-        private readonly string apiUrl = "https://localhost:44376/api/product";
+        private readonly string apiUrl = "http://34.128.144.177/api/product";
 
         public async Task<ActionResult> Index()
         {
